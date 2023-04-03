e88a69f55230be16c1e2678e1275c493129a3db2
Mike <mikepvalente@gmail.com>
2023-04-03 16:07:38 -0300

    Tratamentos de erro adicionado

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -1,11 +1,29 @@
 def soma (a,b):
-    return a+b
+    try:
+        soma= a+b
+    except TypeError:
+        print(f"O input 'a' e 'b' devem ser um float ou int, recebido {a}, tipo(a), {b} tipo (b)")
+    return soma
 
 def subtracao (a,b):
-    return a-b
+    try:
+        subtracao= a-b
+    except TypeError:
+        print(f"O input 'a' e 'b' devem ser um float ou int, recebido {a}, tipo(a), {b} tipo (b)")
+    return subtracao
 
 def divisao(a,b):
-    return a/b
+    try:
+        divisao= a-b
+    except TypeError:
+        print(f"O input 'a' e 'b' devem ser um float ou int, recebido {a}, tipo(a), {b} tipo (b)")
+    except ZeroDivisionError:
+        print(f"O input 'b' não pode ser 0, recebido {b} tipo (b)")
+    return divisao
 
 def multiplicacao(a,b):
-    return a*b
+    try:
+        multiplicacao= a-b
+    except TypeError:
+        print(f"O input 'a' e 'b' devem ser um float ou int, recebido {a}, tipo(a), {b} tipo (b)")
+    return multiplicacao
